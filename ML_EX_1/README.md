5fbd0c80da3ac48bbf46f6bea2a0cad5580e7f3d
polyline <polyline@gapp.nthu.edu.tw>
2017-11-06 11:26:13 +0900

    Completed Homework 1

---
+++ ML_EX_1/README.md
@@ -0,0 +1,4 @@
+# ML_EX1_Housing_Price_prediction
+Homework 1 of ML course of Coursera
+
+Please do not directly use these files as your own homework.
---
+++ ML_EX_1/ex1/gradientDescent.m
@@ -0,0 +1,41 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+    cost_before = computeCost(X, y, theta);
+
+    h = X * theta;
+    theta = theta - ( alpha / m ) * X' * (h - y);
+
+    %Use computeCost function to verify%
+    cost_after = computeCost(X, y, theta);
+    if cost_after - cost_before > 0,
+        disp('Worng!');
+    else,
+        disp('Right');
+    end
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
